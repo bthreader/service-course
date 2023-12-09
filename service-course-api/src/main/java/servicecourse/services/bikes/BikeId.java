8fecd7bce8ae4d7031e32b524997658baef6651b
bthreader <threaderb@hotmail.co.uk>
2023-12-09 20:41:24 +0000

    add javadoc for the two Id Serde classes

--- service-course-api/src/main/java/servicecourse/services/bikes/BikeId.java
+++ service-course-api/src/main/java/servicecourse/services/bikes/BikeId.java
@@ -1,6 +1,13 @@
 package servicecourse.services.bikes;
 
 public class BikeId {
+    /**
+     * De-obfuscates a {@code String} bike ID. Currently, just parses it.
+     *
+     * @param id the bike ID encoded as a String
+     * @return the bike ID
+     * @throws NumberFormatException if the model ID isn't a {@code Long} encoded as a String
+     */
     public static Long deserialize(String id) {
         try {
             return Long.parseLong(id);
@@ -9,6 +16,13 @@ public class BikeId {
         }
     }
 
+    /**
+     * Obfuscates a {@code Long} bike ID. Currently, just turns it into a {@code String} with no
+     * extra steps.
+     *
+     * @param id the bike ID
+     * @return the obfuscated bike ID as a String
+     */
     public static String serialize(Long id) {
         return id.toString();
     }
--- service-course-api/src/main/java/servicecourse/services/models/ModelId.java
+++ service-course-api/src/main/java/servicecourse/services/models/ModelId.java
@@ -1,6 +1,13 @@
 package servicecourse.services.models;
 
 public class ModelId {
+    /**
+     * De-obfuscates a {@code String} model ID. Currently, just parses it.
+     *
+     * @param id the model ID encoded as a String
+     * @return the model ID
+     * @throws NumberFormatException if the model ID isn't a {@code Long} encoded as a String
+     */
     public static Long deserialize(String id) {
         try {
             return Long.parseLong(id);
@@ -9,6 +16,13 @@ public class ModelId {
         }
     }
 
+    /**
+     * Obfuscates a {@code Long} model ID. Currently, just turns it into a {@code String} with no
+     * extra steps.
+     *
+     * @param id the model ID
+     * @return the obfuscated model ID as a String
+     */
     public static String serialize(Long id) {
         return id.toString();
     }
